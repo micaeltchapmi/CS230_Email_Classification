73a2b72d29552073751bad5b0fe195de0c7c4369
BrianLangat <brianlangat11@gmail.com>
2023-04-26 14:05:22 -0700

    Data collection file

---
+++ datacollection.py
@@ -0,0 +1,14 @@
+""""
+Data collection
+    1. Read email
+    2. Label (keep or delete)
+    3. Label (Category e.g 1. Category1, 2. Category 2, etc)
+    4. Saving
+        a) Folder Structure
+           Category1
+           Keep
+           Delete
+        b) Saved Email Format
+           {“timestamp”: time, “Content”: text, “Title:” title, etc}
+
+"""
