4e04f000f1b792d77f6134cdef08aab78b4ee873
Micael Tchapmi <macbook@DNa1c44ce.SUNet>
2023-05-09 00:15:30 -0700

    added primary tab filter

--- datacollection.py
+++ datacollection.py
@@ -1,62 +1,57 @@
 import os
 import json
 from datetime import datetime
-
 import numpy as np
 import imaplib
 import email
-import nltk
-from nltk.tokenize import word_tokenize
 from email.header import decode_header
 import glob
-#from imap_tools import MailBox, AND, A
+import base64
+import html
+import re
 
-# Server is the address of the IMAP server
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from bs4 import BeautifulSoup
 
-
-from sympy import categories
-
-""""
-Data collection
-    1. Read email
-    2. Label (keep or delete)
-    3. Label (Category e.g 1. Category1, 2. Category 2, etc)
-    4. Saving
-        a) Folder Structure
-           Category1
-           Keep
-           Delete
-        b) Saved Email Format
-           {“timestamp”: time, “Content”: text, “Title:” title, etc}
-
-"""
+#TODO:
+# Add ability to relabel some threads if to help correct some of the labels based on our findings if needed
+# Add other email filters like date, time, etc to make labeling easier and more trackable
 
 Email_Categories = {"0": "Passed_Events",
                     "1": "Fufilled_requests",
-                    "2": "Automated_Responses"}
+                    "2": "Automated_Responses",
+                    "3": "Important"
+                    }
 
 Labels = {"0": "Delete",
           "1": "Keep"}
 
 save_dir = "./data"
 
+### Gmail Mailbox credentials ###
+username = "user@gmail.com"
+password = ""
 
-# TODO
-# 1. save labels
-# 2. Read and label emails
-#   a. label as keep (1) or delete(0)
-#   b. label category
-#   c. skip emails that have already been labeled and saved already
-#   d. Skip emails that may be too subjective to reduce bias
-#   e. Add new categories you discover to the Email_Categories list
+#### Gmail API settings ###
 
+credentials_dir = "./credentials"
+path_to_creds = os.path.join(credentials_dir, "gmail_api_creds.json")
+path_to_refresh_token = os.path.join(credentials_dir, "refresh_token.json")
+
+# If modifying these SCOPES, delete the file token.json and generate new one.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# Function to save a labeled email
 def save_label(Email_Data):
     # Email_Data: dictionary containing email content and metadata information for a single email
     # label: 1: keep, 0: delete
     # Category: Number indicating one of the predefined Email Categories
     Category = Email_Categories[Email_Data["Category"]]
     Label = Labels[Email_Data["Label"]]
-    ID = Email_Data["email_id"]
+    ID = Email_Data["thread_id"] + "_" + Email_Data["email_id"]
 
     out_dir = os.path.join(save_dir, Label, Category)
     os.makedirs(out_dir, exist_ok=True)
@@ -65,131 +60,212 @@ def save_label(Email_Data):
     with open(out_path, 'w') as f:
         json.dump(Email_Data, f)
 
+################################################################################################################
+####                    Helper Functions for Reading and Labeling Threads                                   ####
+################################################################################################################
 
-def test_save_label():
-    # TODO: add other fields that could be useful like:
-    # Number of replies, attachments, etc
+### Function to Read gmail API credentials and get Refresh Token ###
+def get_credentials():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(path_to_refresh_token):
+        creds = Credentials.from_authorized_user_file(path_to_refresh_token, SCOPES)
 
-    for i, k in enumerate(range(36)):
-        id = "%05d" % (i)
-        Category = str((i % 3))
-        Label = str((i % 2))
-        Text = "%d message" % (i)
-        Sender = "%i@domain.edu" % (i)
-        data = {"email_id": id,
-                "Category": Category,
-                "Label": Label,
-                "Text": Text,
-                "Sender": Sender
-                }
-        save_label(data)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_config(json.loads(open(path_to_creds, 'r').read()), SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(path_to_refresh_token, 'w') as token:
+            token.write(creds.to_json())
 
+    return creds
 
-# account credentials
-def read_email():
-    username = "brianlangat11@gmail.com"
-    password = ""
+#Function to read all threads from most recent
+def Get_All_Threads():
+    # Get credentials
+    credentials = get_credentials()
 
-    # create an IMAP4 class with SSL
-    imap = imaplib.IMAP4_SSL("imap.gmail.com")
-    # authenticate
-    imap.login(username, password)
+    # Build the Gmail API client
+    service = build('gmail', 'v1', credentials=credentials)
 
-    # select the mailbox I want to delete in
-    # if you want SPAM, use imap.select("SPAM") instead
-    imap.select("INBOX")
-    imap.select('[Gmail]/Primary')
-    status, messages_all = imap.search(None, 'ALL')
-    messages = messages_all[0].split()[-100:]
-    # to get all mails
-    # status, messages = imap.search(None, 'FROM "faridacheptoo@gmail.com"')
-    # print(messages)
-    """"
-    for num in messages[0].split():
-        result, data = imap.fetch(num, '(RFC822)')
-        msg = data[0][1]
-       # print(data[0][1])
+    # Filter threads
+    LABEL_FILTER = "in:inbox category:primary"
+    USERID="me"
+    
 
-        print('Subject:', msg['Subject'])
-        # print('From:', msg['From'])
-        break
-        
-    """
-    """"
-    mb = MailBox('imap.gmail.com').login(username, password, "INBOX")
+    # Call the Gmail API to get the user's primary inbox messages
+    #TODO add more filters e.g date, time of day, month, year, 
+    response = service.users().messages().list(
+        userId=USERID,
+        q=LABEL_FILTER,
+        maxResults=10
+    ).execute()
+    messages = response.get('messages', [])
 
-    # Fetch all unseen emails containing "xyz.com" in the from field
-    # Don't mark them as seen
-    # Set bulk=True to read them all into memory in one fetch
-    # (as opposed to in streaming which is slower but uses less memory)
-
-    # Selecting only email from a specific user
-   
-    messages = mb.fetch(criteria=AND(seegitn=True, from_="faridacheptoo@gmail.com"),
-                        mark_seen=True,
-                        bulk=True)
-  
-    # fetch last 100 emails in inbox
-    messages = mb.fetch(AND(seen=True), limit=10, reverse=True)
-    """
-    """
-    # fetch emails from specific dates
-    # date_lt =Earlier than
-    # date_gte = Within or later than
-    date_obj = datetime.date(2023, 5, 7, 10, 30, 0)
-    messages = mb.fetch(AND(seen=True, date_lt=date_obj), reverse=True)
-    # messages = mb.fetch(A(date_lt=datetime.date(2023, 5, 6), date_gte=datetime.date(2022, 12, 31)))
-    """
-
-    # Adding emails into a dictionary
-    saved_data = {}
-    files = glob.glob(save_dir + "/*/*/*")
-    if len(files) == 0:
-        pass
+    threads = []
+    if not messages:
+        print('No messages found. Check your search criteria')
+        return
     else:
+        thread_ids = []
+        for message in messages:
+            thread_id = message['threadId']
+            if thread_id not in thread_ids:
+                thread_ids.append(thread_id)
 
-        # files = glob.glob(save_dir + "/*/*/*")
+        # Retrieve threads by ID and sort them in the order they are displayed in the user's Gmail account
+        
+        for thread_id in thread_ids:
+            response = service.users().threads().get(
+                userId=USERID,
+                id=thread_id,
+                format='full'
+            ).execute()
+            messages = response['messages']
+            threads.append({'id': thread_id, 'messages': messages})
 
-        # For loop to check if the data is already labelled
-        for file in files:
-            mail = file.split("\\")
-            mailID = mail[-1]
-            mailID_split = mailID.split(".")
-            mailID_num = mailID_split[0]
-            saved_data[mailID_num] = mailID_num
+        threads = sorted(threads, key=lambda t: t['messages'][-1]['internalDate'], reverse=True)
+    
+    return threads
 
-    for msg in messages:
-        if msg.uid in saved_data:
+# Function to get text content from body of email 
+def get_message_body(message):
+    text_parts = []
+    if "parts" in message.keys():
+        message_parts = message['parts']
+        for part in message_parts:
+            if part['mimeType'] == 'text/plain':
+                data = part['body'].get('data')
+                if data:
+                    text = base64.urlsafe_b64decode(data).decode('utf-8')
+                    text = re.sub(r'\[cid:.*?\]', '', text) # remove CID URLs
+                    text_parts.append(text)
+
+            if part.get('parts'):
+                text_parts += get_message_body(part)
+
+    elif "body" in message.keys():
+        data = message['body'].get('data')
+        if data:
+            text = base64.urlsafe_b64decode(data).decode('utf-8')
+            soup = BeautifulSoup(text, 'html.parser')
+            decoded_html = html.unescape(soup.get_text())
+            text = re.sub(r'\[cid:.*?\]', '', decoded_html) # remove CID URLs
+            text_parts.append(text)
+
+    return ''.join(html.unescape(text_parts))
+
+# Function to get header info from email
+def get_header_info(message):
+    subject, sender = "", ""
+    headers = message['headers']
+    for header in headers:
+        if header['name'] == 'Subject':
+            subject = header['value']
+        if header['name'] == 'From':
+            sender = header['value']
+    return subject, sender
+    
+
+################################################################################################################
+####                    Main code for Reading and Labeling Threads                                          ####
+################################################################################################################
+
+# Function to read and label all emails from primary tab
+# Email threads are sorted to be displayed in same order as they appear on gmail
+# Only the first message for each thread is used for labeling
+def Label_Emails():
+    #Get previously labeled threads from disk
+    prev_labeled_threads = set()
+    files = glob.glob(save_dir + "/*/*/*")
+    for file in files:
+        thread_id = file.split("/")[-1].split(".")[0]
+        prev_labeled_threads.add(thread_id)
+
+    # Add previously skipped threads to dictionary
+    skipped_threads_file = "./skipped_threads.txt"
+    prev_skipped_threads = set()
+    skipped_threads = open(skipped_threads_file, 'r').readlines()
+    skipped_threads = [k.strip("\n") for k in skipped_threads] #remove newline characters
+    for s in skipped_threads:
+        prev_skipped_threads.add(s)
+    
+    # Get new threads to label
+    threads = Get_All_Threads()
+
+    # Here is the easy part: We now Label the threads
+    for thread in threads:
+        #get ids
+        first_message = thread['messages'][0]
+        thread_id = first_message["threadId"]
+
+        #skip if thread already labeled or previously skipped
+        if (thread_id in prev_labeled_threads) or (thread_id in prev_skipped_threads):
             continue
 
-        category_label, class_label = label_category(
-            msg)  # Call the function label_category for the user to input the labels in the training data
-        if class_label == -1:
+        message_id = first_message["id"]
+        body = get_message_body(first_message["payload"])
+        subject, sender = get_header_info(first_message["payload"])
+
+        #TODO Later: Get other email info if needed
+
+        # label current thread
+        Class, Category = Label_Thread(sender, subject, body)
+
+        #skip emails that were too subjective and add skipped IDs to file
+        if Class == "-1":
+            prev_skipped_threads.add(thread_id)
+            with open(skipped_threads_file, 'w') as f:
+                for t in prev_skipped_threads:
+                    f.write("%s\n" % (t))
+
             continue
-        Email_Data = {"Category": category_label,
-                      "Label": class_label,
-                      "email_id": msg.uid,
-                      "Text": msg.text}
 
-        save_label(Email_Data)  # Calling save_label with email data
+        Email_Data = {"Category": Category,
+                      "Label": Class,
+                      "thread_id": thread_id,
+                      "email_id": message_id,
+                      "Text": body}
+        
+        #save label
+        save_label(Email_Data)
 
 
-# Define a function to label an email based on its content
-
-def label_category(email):
+# Function to label a thread
+def Label_Thread(sender, subject, body):
     """Prompts the user to label an email"""
-    prompt1 = "Input the category \n 0: Passed_Events, 1: Fufilled_requests, 2: Automated_Responses"
-    prompt2 = "Input the labels \n 0: Delete, 1: Keep  -1: Skip"
+    label_prompt = "Input the label \n 0: Delete, 1: Keep  -1: Skip : "
+    cat_prompt = "Input the category \n"
+    for i in range(len(Email_Categories)):
+        cat_prompt += "%d %s, " % (i, Email_Categories[str(i)])
 
-    print(email.from_, ': ', email.subject)
-    print(email.text)
-    label1 = input(prompt1)
-    label2 = input(prompt2)
-    return label1, label2
+    print("From: " , sender)
+    print("Subject: ", subject)
+    #limit body to 300 characters
+    print(body[0:300]) 
+    Class = input(label_prompt)
+
+    assert Class in ["0", "1", "-1"]
+
+    #no need to label category if we skipped
+    if Class == "-1":
+        return Class, None
+    
+    Category = input(cat_prompt)
+
+    assert Category in Email_Categories.keys()
+
+    return Class, Category
 
 
 def main():
-    read_email()
+    Label_Emails()
 
 
 if __name__ == '__main__':
